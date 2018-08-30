269c610725a4abd78824e169f060f2efc9c8880c
huayifeng <461249104@qq.com>
2018-08-30 23:11:58 +0800

    feature: index

--- index.js
+++ index.js
@@ -1,18 +1,67 @@
-const fs = require('fs');
+const qiniu = require('qiniu');
+const mime = require('mime');
 const path = require('path');
-const qiniuUpload = require('./upload');
-const URL = './img';
+// 文件上传
+function qiniuCloud(config) {
+    if(!config.accessKey || !config.secretKey || !config.bucket) {
+        console.log('params error!');
+        return;
+    }
+    this.accessKey = config.accessKey || '';
+    this.secretKey = config.secretKey || '';
+    this.zone = config.zone || 'Zone_z2';
+    this.bucket = config.bucket || '';
+    this.hostUrl = config.url || '';
+    this.dir = config.dir || '';
+    const options = {
+        scope: this.bucket,
+    };
+    const mac = new qiniu.auth.digest.Mac(this.accessKey, this.secretKey);
+    const putPolicy = new qiniu.rs.PutPolicy(options);
+    this.uploadToken = putPolicy.uploadToken(mac);
+    
+    const qiniuConfig = new qiniu.conf.Config();
+    // 空间对应的机房
+    qiniuConfig.zone = qiniu.zone[this.zone];
+    
+    this.formUploader = new qiniu.form_up.FormUploader(qiniuConfig);
+}
 
-const files = fs.readdirSync(URL);
-const fileList = files.map(item => path.join(URL, item));
+qiniuCloud.prototype.upload = function(list) {
+    const promiselist = list.map((item) => {
+        return this.uploadPromise(item);
+    })
+    return Promise.all(promiselist).then((res) => { 
+        console.log(res);
+        console.log('上传完成!');
+        return res;
+    }).catch(err => {
+        console.log('上传失败!');
+    })
+}
 
-const qiniuO = new qiniuUpload({
-    accessKey: '',
-    secretKey: '',
-    zone: 'Zone_z2',
-    bucket: 'chat',
-    dir: 'test/',
-    url: 'http://pdlu3e6ll.bkt.clouddn.com/',
-})
 
-qiniuO.upload(fileList);
+qiniuCloud.prototype.uploadPromise = function(url) {
+    const name = this.dir + path.basename(url);
+    const mimeType = mime.getType(name);
+    return new Promise((resolve, reject) => {
+        const putExtra = new qiniu.form_up.PutExtra({
+            mimeType,
+        });
+        const _this = this;
+        this.formUploader.putFile(this.uploadToken, name, url, putExtra, function (respErr,
+            respBody, respInfo) {
+            if (respErr) {
+                reject(respErr);
+            }
+            if (respInfo.statusCode == 200) {
+                resolve(_this.hostUrl + name);
+            } else {
+                console.log(respInfo)
+                resolve('error');
+            }
+        });
+    })
+}
+
+module.exports = qiniuCloud;
---
+++ test.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const qiniuUpload = require('./upload');
+const URL = './img';
+
+const files = fs.readdirSync(URL);
+const fileList = files.map(item => path.join(URL, item));
+
+const qiniuO = new qiniuUpload({
+    accessKey: '',
+    secretKey: '',
+    zone: 'Zone_z2',
+    bucket: 'chat',
+    dir: 'test/',
+    url: 'http://pdlu3e6ll.bkt.clouddn.com/',
+})
+
+qiniuO.upload(fileList);
